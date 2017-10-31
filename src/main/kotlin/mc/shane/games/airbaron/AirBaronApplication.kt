6664406bfdf8d6756ba6775751899d260436ae64
jmcshane <jmcshan1@gmail.com>
2017-10-30 19:37:16 -0500

    Adding spring.io generated project

---
+++ src/main/kotlin/mc/shane/games/airbaron/AirBaronApplication.kt
@@ -0,0 +1,11 @@
+package mc.shane.games.airbaron
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AirBaronApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AirBaronApplication::class.java, *args)
+}
